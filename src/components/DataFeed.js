4edf3cd326d8a6d4b0990b620c03e60dd023b592
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2020-11-03 17:20:53 -0500

    Added animation of new DataFeedItems

--- src/components/DataFeed.js
+++ src/components/DataFeed.js
@@ -1,20 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import DataFeedItem from './DataFeedItem';
+import CSSTransition from 'react-transition-group/CSSTransition';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 import './DataFeed.css'
 
 const DataFeed = (props) => {
     const items = props.getAllMetrics()
-        .sort((a, b) => b.date - a.date)
         .map((metric, i) => (
-            <DataFeedItem 
-                {...metric} 
-                key={`${i}_${metric.date.toISOString()}`} />
-		));
-
+            <CSSTransition key={i} timeout={250} classNames="move">
+                <DataFeedItem 
+                    {...metric}
+                    key={`${i}_${metric.date.toISOString()}`} />
+            </CSSTransition>
+        ));
+        
+    // reverse items when presented so that the new items are animated and shown at the top
     return (
         <Container fluid className="DataFeed sticky-top p-3">
             <h3>Data Feed</h3>
-            { items }
+            <TransitionGroup className="items-section__list"> 
+                { items.slice().reverse() }
+            </TransitionGroup>
         </Container>
     );
 };
